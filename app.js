04cf1b1bbcfdaa8cbcfad098f54d3fe41c427f99
Chandler Nova <chandlersnova@gmail.com>
2023-12-19 11:49:01 -0800

    Added security features

--- app.js
+++ app.js
@@ -7,14 +7,18 @@ const logger = require("morgan");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const inventoryRouter = require("./routes/inventory");
+const compression = require("compression");
+const helmet = require("helmet");
 
 const app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB =
+
+const dev_db_url =
   "mongodb+srv://chandlersnova:1.Flannery@cluster0.vsbnbmx.mongodb.net/taking_inventory?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -25,10 +29,18 @@ async function main() {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); // Compress all routes
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
